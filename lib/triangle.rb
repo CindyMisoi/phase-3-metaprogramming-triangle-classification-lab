a9063905dd6903ef206257ee5e39ef5a8acb4fd0
CindyMisoi <crmisoi2k@gmail.com>
2023-02-16 14:17:53 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,41 +1,32 @@
 class Triangle
   # getter and setter
   attr_accessor :side1, :side2, :side3
-  # initialize
-  def initialize(side1,side2,side3)
+   # initialize
+  def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
   end
+  
   def kind
-  #  call validate triangle
-    validate_triangle
-      if @side1 == @side2 && @side2 == @side3 && @side1 == @side3
+    if @side1 > 0 && @side2 > 0 && @side3 > 0 && @side1 + @side2 > @side3 && @side2 + @side3 > @side1 && @side3 + @side1 > @side2
+      if @side1 == @side2 && @side2 == @side3
         return :equilateral
       elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
         return :isosceles
-      else
-        return :scalene 
+      elsif @side1 != @side2 && @side2 != @side3
+        return :scalene
       end
+    else
+      begin
+        raise TriangleError        
+      end 
+    end
   end
 
-    def greater_than_zero?
-      [side1,side2,side3].all?(&:positive?)
-    end
-
-    def  valid_triangle_inequality?
-      side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
-    end
-    
-    def validate_triangle
-      raise TriangleError unless greater_than_zero? && valid_triangle_inequality?
-    end
-
-
-  # defining error class
   class TriangleError < StandardError
     def message
-      puts "Not a triangle"
+      puts "Not a triangle!"      
     end
   end
 
